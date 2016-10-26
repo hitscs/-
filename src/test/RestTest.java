bf885465cd94695be762afe47944dd84080d6930
hitscs <sunchensong@hotmail.com>
2016-10-26 17:25:22 +0800

    httpclient测试方法

---
+++ src/test/RestTest.java
@@ -0,0 +1,86 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+
+public class RestTest {
+
+	@Test
+	public void setDataTest() throws Exception {
+		FileInputStream in = new FileInputStream("f:\\demo.xml");
+		byte[] readBytes = new byte[in.available()];
+		in.read(readBytes);
+		String string4file = new String(readBytes);
+
+		HttpClient client = new HttpClient();
+
+		PostMethod method = new PostMethod("http://localhost:8080/gxdataCenter/searchRest/setData.action");
+		method.addRequestHeader("Accept", "application/json");
+		method.setRequestEntity(new StringRequestEntity(string4file, "text/xml","UTF-8")); 
+        //设置返回的字符编码
+		//client.getParams().setContentCharset("UTF-8");
+		
+		client.executeMethod(method);
+		//String charSet =  method.getResponseCharSet();
+		//String response =new String(method.getResponseBody(),charSet );
+		String response =method.getResponseBodyAsString();
+		System.out.println(response);
+		method.releaseConnection();
+	}
+	@Test
+	public void getNumTest() throws Exception {
+
+		HttpClient client = new HttpClient();
+		
+        //String url = "http://localhost:8080/gxdataCenter/searchRest/getNum.action";
+        String url = "http://localhost:8080/gxdataCenter/searchRest/getNum.json";
+        String param = "keywords="+URLEncoder.encode("", "utf-8")+"";
+        GetMethod method = new GetMethod(url+"?"+param); 	
+
+		method.addRequestHeader("Accept", "application/json");
+        //设置返回的字符编码
+		//client.getParams().setContentCharset("UTF-8");
+		
+		client.executeMethod(method);
+		//String charSet =  method.getResponseCharSet();
+		//String response =new String(method.getResponseBody(),charSet );
+		String response =method.getResponseBodyAsString();
+		System.out.println(response);
+		method.releaseConnection();
+	}
+	@Test
+	public void getTotalNumTest() throws Exception {
+
+		HttpClient client = new HttpClient();
+		
+        //String url = "http://localhost:8080/gxdataCenter/searchRest/getNum.action";
+        String url = "http://localhost:8080/gxdataCenter/searchRest/getTotalNum.action";
+        GetMethod method = new GetMethod(url); 	
+
+		method.addRequestHeader("Accept", "application/json");
+        //设置返回的字符编码
+		//client.getParams().setContentCharset("UTF-8");
+		
+		client.executeMethod(method);
+		//String charSet =  method.getResponseCharSet();
+		//String response =new String(method.getResponseBody(),charSet );
+		InputStream resStream = method.getResponseBodyAsStream();  
+        BufferedReader br = new BufferedReader(new InputStreamReader(resStream));  
+        StringBuffer resBuffer = new StringBuffer();  
+        String resTemp = "";  
+        while((resTemp = br.readLine()) != null){  
+            resBuffer.append(resTemp);  
+        }  
+        String response = resBuffer.toString();  
+		System.out.println(response);
+		method.releaseConnection();
+	}	
+	
+
+}
